4a9441e177ea0b080e2c7e3f8436a9bfa15fd1e1
= <seppi@neureiter.bayern>
2025-10-05 10:19:37 +0200

    stunden constraint fix

--- database/migrations/2025-07-08-060957_init/up.sql
+++ database/migrations/2025-07-08-060957_init/up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS vertretungen (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     klasse VARCHAR(5) NOT NULL,
     stufe SMALLINT CHECK (stufe >= 5 AND stufe <= 13) NOT NULL,
-    stunde SMALLINT CHECK (stunde > 0 AND stunde < 10) NOT NULL,
+    stunde SMALLINT CHECK (stunde > 0 AND stunde <= 12) NOT NULL,
     fach VARCHAR(10),
     fach_neu VARCHAR(10),
     raum VARCHAR(10),
